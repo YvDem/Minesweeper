efc4d68417a36b7e7c431a1fd8ec35b2fae262e8
Yvan <yampleman1401@gmail.com>
2023-03-17 11:43:21 +0100

    modification creation board

--- main.py
+++ main.py
@@ -9,44 +9,49 @@ class Case:
         self.is_revealed = False
         self.is_bomb = False
         self.value = 0
-        self.x = None
-        self.y = None
 
 
 #   size : (x, y)
 class Board:
     def __init__(self, bombs, size):
         self.size = size
-        self.board = np.full((size[0], size[1]), Case())
+        self.board = np.array([[Case() for x in range(size[1])] for y in range(size[0])])
         self.bombs = bombs
 
-
     def create_board(self):
-        for i in range(self.size[1]):
-            for j in range(self.size[0]):
-                self.board[i][j].x = j
-                self.board[i][j].y = i
-        flat_board = self.board.flatten()
+        fl_board = self.board.flatten()
 
-        bombs = 0
-        for case in flat_board:
-            rand = rd.randint(0, bombs)
-            if (rand == 1):
-                bombs += 1
-                case.is_bomb = True
-        for case in flat_board:
-            self._assign_value(self.board, case)
+        if (self.bombs >= len(fl_board)):
+            return False
 
-    def _assign_value(self, board, case):
-        if (case.is_bomb is True):
+        for i in range(self.bombs):
+            fl_board[i].is_bomb = True
+
+        rd.shuffle(fl_board)
+
+        self.board = np.reshape(fl_board, self.size)
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                self._assign_value(x, y)
+
+    def _assign_value(self, x, y):
+        board = self.board
+        if (board[x][y].is_bomb is True):
             return
+        
+        
         
 
     def update_case(self, case, action):
         return
 
     def show_board(self):
+        flat_board = self.board.flatten()
+        for elem in flat_board:
+            print(f'{elem.is_flagged}, {elem.is_revealed}, {elem.is_bomb}, {elem.value}')
         return
+
 
 board = Board(5, (4, 4))
 print(board.create_board())
+board.show_board()
